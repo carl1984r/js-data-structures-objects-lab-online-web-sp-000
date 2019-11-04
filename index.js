553c09b851433a017e1bbd06e2ceb720be745985
Bry Cox <carl.cc@gmail.com>
2019-11-04 11:35:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,3 +11,9 @@ function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
     driver[key] = value;
     return driver;
 }
+
+function deleteFromDriverByKey(driver, key) {
+    const newObj = { ...driver };
+    delete newObj[key];
+    return newObj;
+}
